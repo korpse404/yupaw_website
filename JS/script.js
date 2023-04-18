c3ae1cdd6be8c2ea4fcf67d5961dcf305e4b9c8c
Pavel <vkorpv@gmail.com>
2023-04-18 19:29:58 +0400

    new2

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const bgContainer = document.querySelector('.bg-container');
 const bgLight = document.querySelector('.bg-light');
 
 bgContainer.addEventListener('mousemove', e => {
-    const x = e.clientX;
-    const y = e.clientY;
-    bgLight.style.transform = `translate(${x - 150}px, ${y - 150}px)`;
+  const x = e.clientX;
+  const y = e.clientY;
+  bgLight.style.transform = `translate(${x - 150}px, ${y - 150}px)`;
 });
